5e5512f5fc3c2067c87ad9c91209b1f1922c6549
RaniPatel16 <rani.r.patel.cg@gmail.com>
2026-07-23 13:36:30 +0530

    1456

---
+++ cpp questions/1456.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' ||
+               ch == 'o' || ch == 'u';
+    }
+
+    int maxVowels(const string& s, int k) {
+
+        int left = 0;
+        int right = k - 1;
+
+        int count = 0;
+        int ans = 0;
+
+      
+        for (int i = left; i <= right; i++) {
+            if (isVowel(s[i])) {
+                count++;
+            }
+        }
+
+        ans = count;
+
+      
+        while (right < s.length() - 1) {
+
+        
+            if (isVowel(s[left])) {
+                count--;
+            }
+
+            left++;
+            right++;
+
+            if (isVowel(s[right])) {
+                count++;
+            }
+
+            ans = max(ans, count);
+        }
+
+        return ans;
+    }
+};
